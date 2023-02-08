e3fcdea36b36155238b1b2ffd6bd10e21f3de4de
Alex Kobylansky <alex.kobylansky@gmail.com>
2023-02-08 22:01:48 +0200

    Added CharItem component, and functions to load characters

--- src/components/charList/CharList.tsx
+++ src/components/charList/CharList.tsx
@@ -1,52 +1,65 @@
+import React, {useEffect, useState} from "react";
 import './charList.scss';
-import abyss from '../../assets/img/jpg/abyss.jpg';
+import {getAllCharacters} from "../../services/MarvelService";
 
-const CharList = () => {
-    return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+export const CharList = () => {
+  const [characterList, setCharacterList] = useState({} as ICharacterList);
+  const [offset, setOffset] = useState(9);
+
+  const getCharacters = async () => {
+    await getAllCharacters(0)
+      .then(data => {
+        if (data) {
+          setCharacterList(data);
+        }
+      })
+  };
+
+  useEffect(() => {
+    getCharacters()
+  }, []);
+
+  const loadMore = async () => {
+    setOffset(prevState => prevState + 9);
+
+    await getAllCharacters(offset)
+      .then(data => {
+        if (data) {
+          setCharacterList((prevState: ICharacterList) => {
+            return {
+              ...prevState,
+              data: [...prevState.data, ...data.data]
+            }
+          })
+        }
+      })
+      .catch(error => console.log(error))
+  };
+
+  return (
+    <div className="char__list">
+      <ul className="char__grid">
+        {characterList.data && characterList.data.map((item) => <CharItem key={item.id} thumbnail={item.thumbnail} name={item.name} selected={false}/>)}
+      </ul>
+      <button type="button" className="button button__main button__long" onClick={loadMore}>
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  )
+};
+
+interface CharItemProps {
+  thumbnail: string;
+  name: string;
+  selected: boolean;
 }
 
-export default CharList;
+const CharItem: React.FC<CharItemProps> = ({thumbnail, name, selected}) => {
+
+  return (
+    <li className={"char__item" + `${selected ? " char__item_selected" : ""}`}>
+      <img src={thumbnail} alt="abyss"/>
+      <div className="char__name">{name}</div>
+    </li>
+  )
+};
