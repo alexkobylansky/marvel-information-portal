e72c421e7991a0f7cd22bc95930be757f7ab5e2c
Alex Kobylansky <alex.kobylansky@gmail.com>
2023-02-15 17:46:24 +0200

    Added select character function

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import {AppHeader} from "../appHeader/AppHeader";
 import {RandomChar} from "../randomChar/RandomChar";
 import {CharList} from "../charList/CharList";
@@ -6,14 +7,20 @@ import {CharInfo} from "../charInfo/CharInfo";
 import decoration from "../../assets/img/png/vision.png";
 
 export const App = () => {
+  const [selectedChar, setSelectedChar] = useState<number | null>(null);
+
+  const onCharSelected = (id: number) => {
+    setSelectedChar(id)
+  }
+
     return (
         <div className="app">
             <AppHeader/>
             <main>
                 <RandomChar/>
                 <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
+                    <CharList onCharSelected={onCharSelected} selectedChar={selectedChar}/>
+                    <CharInfo charId={selectedChar}/>
                 </div>
                 <img className="bg-decoration" src={decoration} alt="vision"/>
             </main>
