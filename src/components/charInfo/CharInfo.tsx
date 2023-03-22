21424fcf1f73d0664e26a74e6bf16be9c27572ea
Alex Kobylansky <alex.kobylansky@gmail.com>
2023-03-22 13:31:10 +0200

    Created CharInfo block

--- src/components/charInfo/CharInfo.tsx
+++ src/components/charInfo/CharInfo.tsx
@@ -1,59 +1,97 @@
+import React, {useEffect, useState, useRef} from "react";
 import './charInfo.scss';
-import thor from '../../assets/img/jpg/thor.jpeg';
+import {getCharacter} from "../../services/MarvelService";
+import {Spinner} from "../spinner/Spinner";
+import {Skeleton} from "../skeleton/Skeleton";
 
-export const CharInfo = () => {
-    return (
-        <div className="char__info">
-            <div className="char__basics">
-                <img src={thor} alt="abyss"/>
-                <div>
-                    <div className="char__info-name">thor</div>
-                    <div className="char__btns">
-                        <a href="#" className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href="#" className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div className="char__descr">
-                In Norse mythology, Loki is a god or jötunn (or both). Loki is the son of Fárbauti and Laufey, and the brother of Helblindi and Býleistr. By the jötunn Angrboða, Loki is the father of Hel, the wolf Fenrir, and the world serpent Jörmungandr. By Sigyn, Loki is the father of Nari and/or Narfi and with the stallion Svaðilfari as the father, Loki gave birth—in the form of a mare—to the eight-legged horse Sleipnir. In addition, Loki is referred to as the father of Váli in the Prose Edda.
-            </div>
-            <div className="char__comics">Comics:</div>
-            <ul className="char__comics-list">
-                <li className="char__comics-item">
-                    All-Winners Squad: Band of Heroes (2011) #3
-                </li>
-                <li className="char__comics-item">
-                    Alpha Flight (1983) #50
-                </li>
-                <li className="char__comics-item">
-                    Amazing Spider-Man (1999) #503
-                </li>
-                <li className="char__comics-item">
-                    Amazing Spider-Man (1999) #504
-                </li>
-                <li className="char__comics-item">
-                    AMAZING SPIDER-MAN VOL. 7: BOOK OF EZEKIEL TPB (Trade Paperback)
-                </li>
-                <li className="char__comics-item">
-                    Amazing-Spider-Man: Worldwide Vol. 8 (Trade Paperback)
-                </li>
-                <li className="char__comics-item">
-                    Asgardians Of The Galaxy Vol. 2: War Of The Realms (Trade Paperback)
-                </li>
-                <li className="char__comics-item">
-                    Vengeance (2011) #4
-                </li>
-                <li className="char__comics-item">
-                    Avengers (1963) #1
-                </li>
-                <li className="char__comics-item">
-                    Avengers (1996) #1
-                </li>
-            </ul>
-        </div>
-    )
+interface CharInfoProps {
+  charId: number | null;
 }
+
+export const CharInfo: React.FC<CharInfoProps> = ({charId}) => {
+  const firstInit = useRef(false);
+  const [loading, setLoading] = useState(false);
+  const [character, setCharacter] = useState<ICharacter>({} as ICharacter);
+
+  const updateChar = (charId: number | null) => {
+    if (!charId) {
+      return
+    }
+    setLoading(true);
+    getCharacter(charId)
+      .then((data) => {
+        if (data) {
+          setCharacter(data);
+        } else throw new Error("We couldn't find that character")
+      })
+      .catch((error: any) => console.log(error))
+      .finally(() => {
+        setLoading(false)
+      })
+  };
+
+  useEffect(() => {
+    if (!firstInit.current) {
+      updateChar(charId);
+    }
+    firstInit.current = true;
+  }, []);
+
+  useEffect(() => {
+    updateChar(charId);
+  }, [charId]);
+
+  const skeleton = character.id || loading ? null : <Skeleton/>;
+  const spinner = loading ? <Spinner/> : null;
+  const content = (!loading && character.id) ? <View character={character} /> : null;
+
+  return (
+    <div className="char__info">
+      {skeleton}
+      {spinner}
+      {content}
+    </div>
+  )
+}
+
+interface ViewProps {
+  character: ICharacter;
+}
+
+const View: React.FC<ViewProps> = ({character}) => {
+  const {thumbnail, name, description, homepage, wiki, comics} = character;
+  let imgStyle = {"objectFit": "cover"}
+  if (thumbnail?.includes("image_not_available")) {
+    imgStyle = {"objectFit": "contain"}
+}
+
+  return (
+    <>
+      <div className="char__basics">
+        {/*@ts-ignore*/}
+        <img src={thumbnail} style={imgStyle} alt={name}/>
+        <div>
+          <div className="char__info-name">{name}</div>
+          <div className="char__btns">
+            <a href={homepage} className="button button__main">
+              <div className="inner">Homepage</div>
+            </a>
+            <a href={wiki} className="button button__secondary">
+              <div className="inner">Wiki</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="char__descr">{description ? description : "There is no description for this character"}</div>
+      <div className="char__comics">Comics:</div>
+      <ul className="char__comics-list">
+        {comics.length ? comics.map((item, index) => {
+          if (index >= 10) return;
+          return <li key={index} className="char__comics-item">
+            <a href={item.resourceURI}>{item.name}</a>
+          </li>
+        } ) : "There is no comics with this character"}
+      </ul>
+    </>
+  )
+};
